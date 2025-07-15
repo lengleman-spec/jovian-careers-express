84b8a4872ab039eeb4bde8054f242357e6db5cbd
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2025-07-15 11:31:44 -0400

    added jobs variable

---
+++ public/jobs.js
@@ -0,0 +1,30 @@
+const jobs = [
+  {
+    id: 1,
+    title: "Frontend Developer",
+    location: "Bengaluru, India",
+    salary: "$60,000",
+    posted: "Mar 3, 2023",
+  },
+  {
+    id: 2,
+    title: "Full Stack Developer",
+    location: "New Delhi, India",
+    salary: "$70,000",
+    posted: "Feb 1, 2023",
+  },
+  {
+    id: 3,
+    title: "Data Scientist",
+    location: "San Francisco, USA",
+    salary: "$80,000",
+    posted: "Sep 19, 2023",
+  },
+  {
+    id: 4,
+    title: "Machine Learning Engineer",
+    location: "Remote",
+    salary: "$75,000",
+    posted: "Dec 3, 2023",
+  },
+];
